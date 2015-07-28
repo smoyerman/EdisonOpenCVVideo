09dd603cdba6f465132eade5f5706901611283f5
Stephanie Moyerman <stephanie.moyerman@intel.com>
2015-07-28 11:03:53 -0700

    Initial Commit of Script and Readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # EdisonOpenCVVideo
 Recording a Video with Intel Edison and OpenCV
+
+This script will record a video using a UVC webcam hooked up to Intel Edison
+
+It relies on openCV and its python bindings. 
+
+You can install OpenCV on Edison by first sourcing AlexT's amazing opkg repo:
+
+http://alextgalileo.altervista.org/edison-package-repo-configuration-instructions.html
+
+And then installing with 
+
+$ opkg install python-opencv
+
+Dependencies will be dragged in automatically.  
---
+++ VideoCapture.py
@@ -0,0 +1,28 @@
+# Import Libraries
+import numpy as np
+import cv2
+
+# Define variables
+framerate = 20.0
+videolength = 3
+
+# Grab Camera
+cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.cv.CV_FOURCC(*'XVID')
+out = cv2.VideoWriter('myvideo.avi',fourcc, framerate, (640,480))
+
+# Video part
+for i in range(int(videolength*framerate)):
+    if(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret==True:
+            frame = cv2.flip(frame,0)
+            out.write(frame)
+        else:
+            continue
+
+# Release the camera and video file
+cap.release()
+out.release()
